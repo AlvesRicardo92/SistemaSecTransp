8b728bfa5defb295ceb283457049d6975bdb7f1f
AlvesRicardo92 <ricardobalves3@gmail.com>
2022-06-06 16:15:16 -0300

    teste modal

--- PaginaHome.php
+++ PaginaHome.php
@@ -32,10 +32,12 @@
         </nav>    
     </main>
     <div id="abrirModal" class="modal">
-        <a href="#fechar" title="Fechar" class="fechar">x</a>
-        <h2>Janela Modal</h2>
-        <p>Esta é uma simples janela de modal.</p>
-        <p>Você pode fazer qualquer coisa aqui, página de Login, pop-ups, ou formulários</p>
+        <div>
+            <a href="#fechar" title="Fechar" class="fechar">x</a>
+            <h2>Janela Modal</h2>
+            <p>Esta é uma simples janela de modal.</p>
+            <p>Você pode fazer qualquer coisa aqui, página de Login, pop-ups, ou formulários</p>
+        </div>
     </div>
     <?php
         } else {
