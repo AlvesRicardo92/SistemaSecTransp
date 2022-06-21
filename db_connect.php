4e37271baedcb7ce5d133b8a5d6b086f700baa0c
Ricardo de Barros Alves <87875600+AlvesRicardo92@users.noreply.github.com>
2022-06-21 16:38:22 -0300

    db_connect deletado

--- db_connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-define("HOST", "10.1.0.230"); // O host no qual você deseja se conectar.
-define("USER", "r37484"); // O nome de usuário do banco de dados.
-define("PASSWORD", "r37484"); // A senha do usuário do banco de dados. 
-define("DATABASE", "inventario_informatica_se"); // O nome do banco de dados.
-
-$mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
-// Se você estiver se conectando via TCP/IP ao invés de um socket UNIX, lembre-se de adicionar o número da porta como um parâmetro.
-?>
